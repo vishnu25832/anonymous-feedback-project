3a93a4cb33cf37931eb8f2285ca9a441b6767af5
Vishnu Vardhan <vishnuvardhan25072003@gmail.com>
2026-02-19 19:27:57 +0530

    production API URL + cors fix

--- server/index.js
+++ server/index.js
@@ -14,7 +14,10 @@ const app = express();
 // MIDDLEWARE
 // ==============================
 app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000"
+    origin: [
+        "http://localhost:3000",
+        "https://anonymous-feedback-client.onrender.com"
+    ]
 }));
 
 
